b81a167b0f2564cb60926d1434053dc5e3ba98cb
albert-wb <popkingof4@gmail.com>
2026-06-01 21:16:18 -0300

    Add authenticated routes and update build/runtime
    Add new authenticated subpage routes (tasks, social, turmas, history) and icon.png to Next.js build manifests and server app-paths. Update the RSC client-reference manifest to include client/module mappings for the new pages. Introduce webpack AMD runtime option and bump the runtime hash; reorder vendor chunk list in the authenticated page bundle. These are build output changes after adding the new pages.

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("6abfac53c27d2651")
+/******/ 		__webpack_require__.h = () => ("d1739cb997a0f0e5")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
